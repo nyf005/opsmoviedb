dbe64447c4ce68fc38b3c2c3a3f6ab68bb52b2b6
Fabrice N'DRI <nyf005@gmail.com>
2018-04-28 16:49:50 +0000

    Adding new file App.js for heroku

---
+++ App.js
@@ -0,0 +1,11 @@
+{
+    "name": "movie_db_webhook",
+    "description": "A very simple webhook that gets you the details of a specified movie.",
+    "repository": "https://github.com/nyf005/opsmoviedb.git",
+    "keywords": [
+        "movies",
+        "api.ai",
+        "chatbot",
+        "webhook"
+    ]
+}
